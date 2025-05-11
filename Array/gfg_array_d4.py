955901570e85a43d00888605e0f9c60248e880a2
AJESH <158215604+ajesh123km@users.noreply.github.com>
2025-05-11 23:59:49 +0530

    Update gfg_array_d4.py

--- Array/gfg_array_d4.py
+++ Array/gfg_array_d4.py
@@ -8,7 +8,7 @@ class Solution:
         d%=n
         temp=[0]*n  
         for i in range(n-d):
-            temp[i]=arr[d+i]
+            temp[i]=arr[d+i]   
         for i in range(d):
             temp[n-d+i]=arr[i]
         for i in range(n):
