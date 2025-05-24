7c3b6658bc4386ff748cefbe80178ba34c7b993b
AJESH <158215604+ajesh123km@users.noreply.github.com>
2025-05-24 13:21:07 +0530

    Update gfg_string_d20_Strings Rotations of Each Other.py

--- String/gfg_string_d20_Strings Rotations of Each Other.py
+++ String/gfg_string_d20_Strings Rotations of Each Other.py
@@ -2,4 +2,5 @@ def areRotations(s1, s2):
     return len(s1) == len(s2) and s2 in (s1 + s1)
 s1 = "aab"
 s2 = "aba"
-print("true" if areRotations(s1, s2) else "false")
+print("true" if areRotations(s1, s2) else "false")\
+
