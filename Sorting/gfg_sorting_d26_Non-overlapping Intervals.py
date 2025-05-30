7fa0cfd6c4471a2e9f8ecf657f035281c0cac676
AJESH <158215604+ajesh123km@users.noreply.github.com>
2025-05-30 21:58:59 +0530

    Update gfg_sorting_d26_Non-overlapping Intervals.py

--- Sorting/gfg_sorting_d26_Non-overlapping Intervals.py
+++ Sorting/gfg_sorting_d26_Non-overlapping Intervals.py
@@ -4,7 +4,7 @@ def minRemoval(intervals):
     end = intervals[0][1]
 
     for i in range(1, len(intervals)):
-        if intervals[i][0] < end:
+        if intervals[i][0] < end: 
             cnt += 1
             end = min(intervals[i][1], end)
         else:
