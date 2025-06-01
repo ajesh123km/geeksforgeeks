7b0400631fa0cd55248815f5447f95142835c1a6
AJESH <158215604+ajesh123km@users.noreply.github.com>
2025-06-01 23:18:01 +0530

    Add files via upload

---
+++ searching/gfg_searching_d28_Number_of_occurrence.py
@@ -0,0 +1,11 @@
+from bisect import bisect_left, bisect_right
+
+def countFreq(arr, target):
+    l = bisect_left(arr, target)
+    r = bisect_right(arr, target)
+    return r - l
+
+if __name__ == "__main__":
+    arr = [1, 2, 2, 2, 2, 3, 4, 7, 8, 8]
+    target = 2
+    print(countFreq(arr, target))
