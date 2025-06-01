6e77813b97a8b755a6ac9391f91af17fafcc3cee
AJESH <158215604+ajesh123km@users.noreply.github.com>
2025-06-01 23:18:19 +0530

    Update gfg_searching_d28_Number_of_occurrence.py

--- searching/gfg_searching_d28_Number_of_occurrence.py
+++ searching/gfg_searching_d28_Number_of_occurrence.py
@@ -4,6 +4,7 @@ def countFreq(arr, target):
     l = bisect_left(arr, target)
     r = bisect_right(arr, target)
     return r - l
+    
 
 if __name__ == "__main__":
     arr = [1, 2, 2, 2, 2, 3, 4, 7, 8, 8]
