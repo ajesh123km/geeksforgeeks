d5cad7a73a0662f18a7e1565e1d201d9cabc2d6e
AJESH <158215604+ajesh123km@users.noreply.github.com>
2025-06-01 23:12:46 +0530

    Create gfg_searching_d28_Number of occurrence.py

---
+++ searching/gfg_searching_d28_Number of occurrence.py
@@ -0,0 +1,10 @@
+def countFreq(arr, target):
+    res = 0
+    for i in range(len(arr)):
+        if arr[i] == target:
+            res += 1
+    return res
+
+arr = [1, 2, 2, 2, 2, 3, 4, 7, 8, 8]
+target = 2
+print(countFreq(arr, target))
