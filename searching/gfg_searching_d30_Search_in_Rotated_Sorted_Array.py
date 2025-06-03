5541794a7616008b1062ff004a5ba4627133c290
AJESH <158215604+ajesh123km@users.noreply.github.com>
2025-06-03 12:14:40 +0530

    Add files via upload

---
+++ searching/gfg_searching_d30_Search_in_Rotated_Sorted_Array.py
@@ -0,0 +1,27 @@
+def search(arr, key):
+    lo = 0
+    hi = len(arr) - 1
+
+    while lo <= hi:
+        mid = lo + (hi - lo) // 2
+
+        if arr[mid] == key:
+            return mid
+
+        if arr[mid] >= arr[lo]:
+            if key >= arr[lo] and key < arr[mid]:
+                hi = mid - 1
+            else:
+                lo = mid + 1
+        else:
+            if key > arr[mid] and key <= arr[hi]:
+                lo = mid + 1
+            else:
+                hi = mid - 1
+
+    return -1
+
+if __name__ == "__main__":
+    arr1 = [5, 6, 7, 8, 9, 10, 1, 2, 3]
+    key1 = 3
+    print(search(arr1, key1))
