4f9c59c71175d701ba408b8c8e012f782b0b3101
AJESH <158215604+ajesh123km@users.noreply.github.com>
2025-06-04 13:26:32 +0530

    Add files via upload

---
+++ searching/gfg_searching_d31_Peak_element.py
@@ -0,0 +1,19 @@
+def peakElement(arr):
+    n = len(arr)
+
+    for i in range(n):
+        left = True
+        right = True
+
+        if i > 0 and arr[i] <= arr[i - 1]:
+            left = False
+
+        if i < n - 1 and arr[i] <= arr[i + 1]:
+            right = False
+
+        if left and right:
+            return i
+
+if __name__ == "__main__":
+    arr = [1, 2, 4, 5, 7, 8, 3]
+    print(peakElement(arr))
