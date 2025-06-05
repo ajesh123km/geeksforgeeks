7c7333b001d66316b45c31077e2e72b93913ab68
AJESH <158215604+ajesh123km@users.noreply.github.com>
2025-06-05 08:37:25 +0530

    Add files via upload

---
+++ searching/gfg_searching_d32_K-th element of two Arrays.py
@@ -0,0 +1,18 @@
+def kthElement(a, b, k):
+    arr = []
+    
+    for i in a:
+        arr.append(i)
+    
+    for i in b:
+        arr.append(i)
+    
+    arr.sort()
+    
+    return arr[k - 1]
+
+if __name__ == "__main__":
+    a = [2, 3, 6, 7, 9]
+    b = [1, 4, 8, 10]
+    k = 5
+    print(kthElement(a, b, k))
