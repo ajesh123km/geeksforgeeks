d7ac880ed3f41c465cd48b2b5bdfd69aab860ae2
AJESH <158215604+ajesh123km@users.noreply.github.com>
2025-06-08 14:19:23 +0530

    Add files via upload

---
+++ searching/gfg_searching_d35_Kth_Missing_Positive_Number_in_a_Sorted_Array.py
@@ -0,0 +1,16 @@
+def kthMissing(arr, k):
+    s = set(arr)
+    count = 0
+    curr = 0
+
+    while count < k:
+        curr += 1
+        if curr not in s:
+            count += 1
+    
+    return curr
+
+if __name__ == "__main__":
+    arr = [2, 3, 4, 7, 11]
+    k = 5
+    print(kthMissing(arr, k))
